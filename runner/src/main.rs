64d535dafd6809b89077561a7e4f260104bd9104
pierre <dommerc.pierre@gmail.com>
2023-10-05 16:01:04 +0200

    wip

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,17 +1,17 @@
 use libloading::{Library, Symbol};
-use std::env::consts::OS;
+
+#[cfg(target_os = "linux")]
+const LIB_FILE: &str = "libvpnym.so";
+#[cfg(target_os = "windows")]
+const LIB_FILE: &str = "vpnym.dll";
+#[cfg(target_os = "macos")]
+const LIB_FILE: &str = "vpnym.dylib";
 
 fn call_dynamic() -> Result<(), Box<dyn std::error::Error>> {
-    let lib_file = match OS {
-        "windows" => "vpnym.dll",
-        "macos" => "vpnym.dylib",
-        _ => "libvpnym.so",
-    };
-    let lib_path = format!("lib/{lib_file}");
+    let lib_path = format!("lib/{LIB_FILE}");
     println!("lib_path: {}", lib_path);
     unsafe {
         let lib = Library::new(lib_path)?;
-        // let lib = Library::new("libvpnym.so")?;
         let func: Symbol<unsafe extern "C" fn()> = lib.get(b"run_tauri")?;
         func();
         Ok(())
